d32b2908b90e662e287eff7b5b75bad1398975d1
CrazyGrin <894355833@qq.com>
2017-04-04 22:26:40 +0800

    add es5 to es6 and less to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-
-gulp.task('default', function() {
-  console.log("hello world");
-});
+var gulp = require('gulp'),
+	less = require('gulp-less'),
+	babel = require('gulp-babel')
+	browserSync = require('browser-sync').create();
 
 gulp.task('watch', () => {
     browserSync.init({
@@ -11,6 +9,22 @@ gulp.task('watch', () => {
             baseDir: 'server/'
         }
     });
+    gulp.watch('./server/less/**.less', ['less']);
     gulp.watch('./server/*').on('change', browserSync.reload);
     gulp.watch('./server/index.html').on('change', browserSync.reload);
+    gulp.watch('./server/es6/*.js', ['babel']);
+});
+
+gulp.task('less', () => {
+    gulp.src('server/less/**.less')
+        .pipe(less())
+        .pipe(gulp.dest('./server/css'));
+});
+gulp.task('babel', () => {
+	console.log(123);
+	gulp.src('./server/es6/*.js')
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+		.pipe(gulp.dest('./server/es5/')) ;
 });
